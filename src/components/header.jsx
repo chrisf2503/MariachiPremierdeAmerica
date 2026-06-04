fd74b8231c114726260f3939b559764889a23f1c
Christopher Flores <Christopher321flores@gmail.com>
2026-06-04 19:54:28 -0400

    adding foundation

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return (
+        <header>
+            <img src="" alt="" />
+            <h2>Mariachi Premier de America</h2>
+            <p>Música Auténtica de Mariachi Mexicano para Tus Eventos Especiales</p>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/home.jsx
@@ -0,0 +1,20 @@
+import Header from './components/header.jsx'
+function Home(){
+
+    return (
+        <main className="body">
+            {/* 
+                Layout is as follows:
+                Header
+                Brief History
+                Video Container
+                Photo Gallary
+                Form 
+                Footer 
+            */}
+           <Header/>
+        </main>
+    )
+}
+
+export default Home;
