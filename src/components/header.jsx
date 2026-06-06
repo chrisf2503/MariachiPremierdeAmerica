b11fcec763a24ecdcb2995a11d4a2deaa4e4ec20
Christopher Flores <Christopher321flores@gmail.com>
2026-06-05 22:53:51 -0400

    setting up header component for the website

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,23 @@
+import logo from "../assets/PremierdeAmericaLogo.jpg"
+import instagramIcon from '../assets/Instagram.svg'
+import FacebookIcon from '../assets/Facebook.svg'
+import PhoneIcon from '../assets/Phone.svg'
+import headerStyle from '../CSS/header.module.css'
+const socialMedia = [
+    {id: "facebook", img: {FacebookIcon}, link: ""},
+    {id: "instagram", img: {instagramIcon}, link: ""},
+    {id: "phone", img: {PhoneIcon}, link: ""},
+]
 function Header(){
     return (
-        <header>
-            <img src="" alt="" />
-            <h2>Mariachi Premier de America</h2>
-            <p>Música Auténtica de Mariachi Mexicano para Tus Eventos Especiales</p>
+        <header className={headerStyle.header}>
+            <img src={logo} alt="MairachiPremierdeAmericaLogo" className ={headerStyle.logo}/>
+            <h1 className={headerStyle.title}>Mariachi Premier de America</h1>
+            <p className={headerStyle.discription}>Música Auténtica de Mariachi Mexicano para Tus Eventos Especiales</p>
+            <div className="socials">
+                {//adding loop here
+                }
+            </div>
         </header>
     )
 }
